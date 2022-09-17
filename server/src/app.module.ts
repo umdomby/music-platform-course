df9c11d71601999fce4c60250c31a0d8261b309b
navatar <umdomby@yandex.ru>
2022-09-17 05:03:44 +0300

    first commit

---
+++ server/src/app.module.ts
@@ -0,0 +1,17 @@
+import {Module} from "@nestjs/common";
+import {TrackModule} from "./track/track.module";
+import {MongooseModule} from "@nestjs/mongoose";
+import {FileModule} from "./file/file.module";
+import * as path from 'path'
+import {ServeStaticModule} from "@nestjs/serve-static";
+
+@Module({
+    imports: [
+        ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
+        //MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.oeudk.mongodb.net/music-platform?retryWrites=true&w=majority'),
+        MongooseModule.forRoot('mongodb://localhost:27017/music'),
+        TrackModule,
+        FileModule
+    ]
+})
+export class AppModule {}
